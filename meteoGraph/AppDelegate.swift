f4cb250a936aa200e17fd0f3bc9b6462e79a335f
Your name <your.email@example.com>
2019-07-24 11:57:21 +0200

    Initial Commit

---
+++ meteoGraph/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  meteoGraph
+//
+//  Created by thierry hentic on 24/07/2019.
+//  Copyright © 2019 thierry hentic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
