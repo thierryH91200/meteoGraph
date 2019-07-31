362d864c54736f0166ed4bf4d4325b96334d133a
Your name <your.email@example.com>
2019-07-31 09:32:21 +0200

    init

--- meteoGraph/Controller/Util/FlickrProvider.swift
+++ meteoGraph/Controller/Util/FlickrProvider.swift
@@ -17,21 +17,16 @@ class FlickrProvider {
     struct Keys {
         static let flickrKey = "96d2fddc4b9ed903c513bf9aa16ed6e9"
     }
-    
-    let apiFlickrKey = "96d2fddc4b9ed903c513bf9aa16ed6e9"
-    let secretFlickrKey = "57994500978b8f1e"
-    
+        
     struct Errors {
         static let invalidAccessErrorCode = 100
     }
     
     class func fetchPhotosForSearchText(location: CLLocationCoordinate2D, cityName: String, cityID: String, onCompletion: @escaping FlickrResponse) -> Void {
-        
-        let apiFlickrKey = "96d2fddc4b9ed903c513bf9aa16ed6e9"
-        
+                
         var urlComponents = URLComponents(string: "https://api.flickr.com/services/rest/")!
         urlComponents.queryItems = [
-            URLQueryItem(name: "api_key", value: apiFlickrKey),
+            URLQueryItem(name: "api_key", value: Keys.flickrKey),
             
             URLQueryItem(name: "method", value: "flickr.photos.search"),
             URLQueryItem(name: "safe_search", value: "1"),
@@ -48,19 +43,19 @@ class FlickrProvider {
             URLQueryItem(name: "format", value: "json"),
             URLQueryItem(name: "nojsoncallback", value: "1")
         ]
-        let request = URLRequest(url: urlComponents.url!)
         
+        let request = URLRequest(url: urlComponents.url!)
         let searchTask = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error -> Void in
             
-            if error != nil {
+            guard error == nil else {
                 onCompletion(error as NSError?, nil)
                 return
             }
+            
             do {
                 
                 let decoder = JSONDecoder()
                 let gitData = try decoder.decode(FilterResult.self, from: data!)
-                
                 let photosArray = gitData.photos.photo
                 
                 let flickrPhotos: [FlickrPhoto] = photosArray.map { photoDictionary in
@@ -82,18 +77,16 @@ class FlickrProvider {
                 onCompletion(error, nil)
                 return
             }
-            
         })
         searchTask.resume()
     }
     
     class func fetchPhotosForGetSize( photo_id : String, onCompletion: @escaping FlickrSizeResponse) -> Void {
-        
-        let apiFlickrKey = "96d2fddc4b9ed903c513bf9aa16ed6e9"
-        
+                
         var urlComponents = URLComponents(string: "https://api.flickr.com/services/rest/")!
         urlComponents.queryItems = [
-            URLQueryItem(name: "api_key", value: apiFlickrKey),
+            URLQueryItem(name: "api_key", value: Keys.flickrKey),
+            
             URLQueryItem(name: "method", value: "flickr.photos.getSizes"),
             
             URLQueryItem(name: "photo_id", value: photo_id),
@@ -102,46 +95,43 @@ class FlickrProvider {
             URLQueryItem(name: "nojsoncallback", value: "1")
             
         ]
+        
         let request = URLRequest(url: urlComponents.url!)
-        print(request.description)
         let searchTask = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error -> Void in
             
-            if error != nil {
+            guard error == nil else {
                 onCompletion(error as NSError?, nil)
                 return
             }
+            
             do {
                 
                 let decoder = JSONDecoder()
                 let gitData = try decoder.decode(SizeResult.self, from: data!)
+                let sizeArray = gitData.sizes.size
                 
-                let photosArray = gitData.sizes.size
-                
-                let flickrPhotos: [SizePhoto] = photosArray.map { photoDictionary in
+                let sizeFlickrPhotos: [SizePhoto] = sizeArray.map { photoDictionary in
                     
                     let label = photoDictionary.label
-                    //                    let width = photoDictionary.width
-                    //                    let height = photoDictionary.height
+//                    let width = photoDictionary.width
+//                    let height = photoDictionary.height
                     let source = photoDictionary.source
                     let url = photoDictionary.url
                     let media = photoDictionary.media
                     
-                    //                    let flickrPhoto = SizePhoto(label: label, height: height, source: source, url: url, media: media)
+//                    let flickrPhoto = SizePhoto(label: label, height: height, source: source, url: url, media: media)
                     let flickrPhoto = SizePhoto( label: label, source: source, url: url, media: media)
                     return flickrPhoto
                 }
-                onCompletion(nil, flickrPhotos)
+                onCompletion(nil, sizeFlickrPhotos)
                 
             } catch let error as NSError {
                 print("Error parsing JSON: \(error)")
                 onCompletion(error, nil)
                 return
             }
-            
         })
         searchTask.resume()
-        
-        
     }
     
 }
